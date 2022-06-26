ce212269b4db643f6e10c0dfc6ca42dd3245f3e3
Вячеслав Жуков <zhukovvv@bk.ru>
2022-06-27 02:11:31 +0300

    Proekt1

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,7 +6,7 @@ Group = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     slug = models.SlugField()
     description = models.TextField(blank=True, null=True)
 
